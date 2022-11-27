922b41325a757add122b4b8e2eb47fb9ce644430
Lucio Studer <lucio.studer@gmail.com>
2022-11-27 18:19:28 +0000

    atualizada equipa

--- mentha/models.py
+++ mentha/models.py
@@ -29,7 +29,7 @@ class Contacto(models.Model):
 class Noticia(models.Model):
 
 
-    titulo = models.CharField(max_length=60, null=False, blank=False)
+    titulo = models.CharField(max_length=120, null=False, blank=False)
     texto = models.TextField(null=False, blank=False)
     data = models.DateField(null=True)
     url = models.URLField(null=True, blank=True)
